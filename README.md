d333a2db53cdf08379987b75680506f16bd6d085
AstronomyiPhone <cambernhardt@me.com>
2014-02-28 16:46:17 -0500

    Added Python file and updated README
    Python file shows question, question classification, directions, and
    possible answers. See README for upcoming features.

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
 SAT QOTD
 =======
 
-Shows SAT Question of the Day and response statistics from the College Board.
+Shows SAT Question of the Day, question classification, possible answers, and response statistics from the College Board.
+
+Includes HTML file that uses iframes and a Python file*; both give an output like this:
+
+                   ----SAT QOTD----
+----Last updated: Fri Feb 28 2014 21:19:10 GMT+0000 (UTC)----
+
+Hello. Welcome to your daily SAT training. Today is Feb 28. Your mission, should you choose to accept it, will be to answer this question, which is classified as: Critical Reading > Sentence Completions.
+
+Warning. This mission is dangerous. 35,030 people have tried and 66% of them have survived (okay, gotten it correct).
+
+Here is your briefing:
+
+The ballet stage is a bright, seemingly weightless world where gravity is continually being ------- by the dancers.
+(A) prolonged
+(C) unbalanced
+(D) reflected
+(E) defied
 
 
 SAT is a registered trademark of the College Board, which does not endorse or support this project.
 
-Progress:
+Checklist:
+-Fix input mechanism for Python
+-Add answering ability for Python
+-Add answer checking and explanation for Python
+-Cleanup interface for HTML
 
--Posted HTML with iframe showing data collected by API created with Kimono
 
--Plan on using JS to create a program to collect data and display it in a nicer format
+*Working on a proper input mechanism, which is currently commented out in the Python file; if you want to give it a go, feel free to do so.
---
+++ SATQOTD.py
@@ -0,0 +1,105 @@
+import json
+import urllib
+
+results = json.load(urllib.urlopen("http://www.kimonolabs.com/api/a9tmzsfm?apikey=74ef20963a82fa61fe92928fd750f7ce"))
+
+print "                   ----" + results['name'] + "----"
+print "----Last updated: " + results['lastsuccess'] + "----\n"
+
+results = results['results']
+qresults = results['Question Data']
+first_results = qresults[0]
+
+date = first_results['Date']
+directions = first_results['Directions']
+
+
+second_results = qresults[1]
+stats = second_results['Stats']
+
+third_results = qresults[2]
+typeques = third_results['Type of Question']
+question = third_results['Question']
+tries = ""
+percent = ""
+
+checking = True
+i = 0
+x = 0
+
+while checking:
+	x = stats[i]
+	if x != " ":
+		tries += x
+	else:
+		checking = False
+	i += 1
+
+checking = True
+now_checking = False
+
+i = 0
+y = 0
+
+while checking: #This gets a bit complicated; the data I'm parsing reads "xx,xxx responses \nxx% correct"; I'm looking for the 'xx%', so I want to start adding to percent if it's after \n and stop at the space after the '%'; probably a better way to do this
+	for x in stats:
+		if x == "\n" and y > 0:
+			now_checking = True
+		elif x == "\n":
+			y += 1
+		if now_checking:
+			if x == " ":
+				now_checking = False
+				checking = False
+			if now_checking:
+				percent += x
+
+temp = ""
+
+percent = percent.split()
+
+for i in percent:
+	if i != " " and i != "\n":
+		temp += i
+
+percent = str(temp)
+
+
+temp = ""
+
+for x in date:
+	if x != "\n":
+		temp += x
+	else:
+		temp += " "
+
+date = temp
+
+
+
+print "Hello. Welcome to your daily SAT training. Today is " + date + ". Your mission, should you choose to accept it, will be to answer this question, which is classified as: " + typeques + ".\n"
+print "Warning. This mission is dangerous. " + tries + " people have tried and " + percent + " of them have survived (okay, gotten it correct).\n"
+print "Here is your briefing: \n" + directions
+
+validans = False
+resp = True
+
+#while not(validans):
+#	start = raw_input("Ready to begin? Type yes or no.")
+#	if start.upper() == 'YES':
+#		validans = True
+#		resp = True
+#	elif start.upper() == 'NO':
+#		validans = True
+#		resp = False
+#	else:
+#		print "Sorry. Try again:\n"
+
+
+if resp == True:
+	print question
+
+aresults = results["Answer Data"]
+
+for i in aresults:
+	print i['Answers']
