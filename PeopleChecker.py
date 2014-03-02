da0654f72e2f742e1eb2a939dd2ba9df108aa354
AstronomyiPhone <cambernhardt@me.com>
2014-03-02 11:42:20 -0500

    Added time checking abilities
    Now gets time when people limit reached and optionally displays it in
    your timezone

--- PeopleChecker.py
+++ PeopleChecker.py
@@ -4,31 +4,49 @@ import time
 
 people = 0
 
-check_num = 3000 #raw_input("Enter number of people to check for")
 check_num = 4000 #or raw_input("Enter number of people to check for")
 
-while people < check_num:
-	results = json.load(urllib.urlopen("http://www.kimonolabs.com/api/a9tmzsfm?apikey=74ef20963a82fa61fe92928fd750f7ce"))
+#while people < check_num:
+#	results = json.load(urllib.urlopen("http://www.kimonolabs.com/api/a9tmzsfm?apikey=74ef20963a82fa61fe92928fd750f7ce"))
+#
+#	results = results['results']
+#	results = results['Question Data']
+#	time.sleep(2)
+#	results = results[1]
+#
+#	stats = results['Stats']
+#
+#	temp = ""
+#	checking = True
+#	i = 0
+#
+#	while checking:
+#		x = stats[i]
+#		if x != " ":
+#			temp += x
+#		else:
+#			checking = False
+#		i += 1
+#		time.sleep(3)
+#
+#	stats = temp
+#	temp = ""
+#
+#	for j in stats:
+#		if j != ",":
+#			temp += j
+#
+#	people = int(temp)
+#	time.sleep(5)
 
-	results = results['results']
-	results = results['Question Data']
-	time.sleep(1.5)
-	results = results[1]
-
-	stats = results['Stats']
 results = json.load(urllib.urlopen("http://www.kimonolabs.com/api/a9tmzsfm?apikey=74ef20963a82fa61fe92928fd750f7ce"))
 finished = results['lastsuccess']
 
-	temp = ""
-	checking = True
-	i = 0
 temp = ""
 checking = True
 i = 0
 spaces = 0
 
-	while checking:
-		x = stats[i]
 while checking: #data read in format "day month date year hh:mm:ss GMT+(-)xxxx (timezone)" -> once four spaces have registered, it reads in the time to temp until it hits another space before the GMT offset
 	x = finished[i]
 	if spaces > 3:
@@ -36,30 +54,19 @@ while checking: #data read in format "day month date year hh:mm:ss GMT+(-)xxxx (
 			temp += x
 		else:
 			checking = False
-		i += 1
-		time.sleep(2)
 	else:
 		if x == " ":
 			spaces += 1
 	i += 1
 	#time.sleep(3)
 
-	stats = temp
-	temp = ""
 finished = temp
 
-	for j in stats:
-		if j != ",":
-			temp += j
 finished = finished.split(":") #time is in format hh:mm:ss, gives array [hh, mm, ss]
 
-	people = int(temp)
-	time.sleep(5)
 x = ""
 time_ready = []
 
-results = json.load(urllib.urlopen("http://www.kimonolabs.com/api/a9tmzsfm?apikey=74ef20963a82fa61fe92928fd750f7ce"))
-print results['lastsuccess']
 for x in finished:
 	time_ready.append(int(x))
 
